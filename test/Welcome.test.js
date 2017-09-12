f7c5345e7fc947ec300f0f2c018295f5c9548907
Francy Lang <francy.lang@gmail.com>
2017-09-12 15:23:27 -0600

    added API key.js and put it in gitignore file

---
+++ test/Welcome.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Welcome from '../lib/Welcome.js';
+import { shallow, mount } from 'enzyme';
+
+describe('Welcome funcitionality', () => {
+  it('should render a welcome page', () => {
+    
+  })
+
+}

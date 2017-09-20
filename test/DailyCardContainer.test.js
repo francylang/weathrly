a26a04061115cd6494cdb3535826064bd27af020
Jason Hughes <jasonhughes088@gmail.com>
2017-09-20 12:44:36 -0600

    a few search tests working

--- test/DailyCardContainer.test.js
+++ test/DailyCardContainer.test.js
@@ -3,14 +3,14 @@ import DailyCardContainer from '../lib/Components/DailyCardContainer/DailyCardCo
 import { shallow, mount } from 'enzyme';
 
 
-const mockData = {
+let mockData = {
   day: {
     tenDayCondition: ['Thunderstorm', 'Rain'],
     tenDayDay: ['Tue', 'Wed'],
     tenDayHigh: ['86', '82'],
     tenDayIcon: ['tstorms', 'rain'],
     tenDayLow: ['79', '79'],
-  },
+  }
 
 };
 
@@ -19,16 +19,18 @@ describe('Card Functionality', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(
+     wrapper = mount(
        <DailyCardContainer
          {...mockData}
        />
      );
-  });
+   });
 
 
-  it('it should render multiple DailyCards', () => {
-    const dailyCards = wrapper.find('DailyCard');
-    expect(dailyCards.length).toEqual(2);
-  });
+   it('it should render multiple DailyCards', () => {
+     const dailyCards = wrapper.find('DailyCard');
+     expect(dailyCards.length).toEqual(2);
+   });
+
+
 });
--- test/Search.test.js
+++ test/Search.test.js
@@ -4,8 +4,11 @@ import { shallow, mount } from 'enzyme';
 import Current from '../lib/Components/Current/Current.js';
 import Welcome from '../lib/Components/Welcome/Welcome.js';
 import App from '../lib/Components/App/App.js';
+import Search from '../lib/Components/Search/Search.js'
 
 describe('SEARCH', () => {
+  let wrapper;
+  let mkFun;
   global.localStorage = {
     getItem: () => false,
   };
@@ -16,10 +19,78 @@ describe('SEARCH', () => {
 });
   });
 
+  it('should render an input field', () => {
+    const search = shallow(<Search />);
+    expect(search.find('.search')).toBeDefined();
+  });
+
+
+
+
+
+
+
+
+
+//   it('should change state based on the input', () => {
+//      const search = mount(<Search />);
+//      const input = search.find('input');
+//      expect(search.state('location')).toEqual('');
+//      const inputValue = { target: { value: 'Baton Rouge' } };
+// })
+
+
+  // it('should change state based on the input', () => {
+  //   const search = mount(<Search />);
+  //   const input = search.find('input');
+  //
+  //
+  //   expect(search.state('input')).toEqual('');
+  //   const inputValue = { target: { value: 'louisville, ky' } };
+  //   input.simulate('change', inputValue);
+  //   expect(search.state('location')).toEqual('louisville, ky');
+  // });
+
+
+
+
+
 
   it('should render the search bar when displaying weather report', () => {
     const component = mount(<App />);
     const current = component.find('Search');
     expect(current.nodes.length).toEqual(1);
   });
+
+  it('should render a button field', () => {
+    const search = shallow(<Search />);
+    expect(search.find('button')).toBeDefined();
+  });
+
+  it('fire a function on click', () => {
+    const mkFun = jest.fn()
+    const search = mount(<Search handleSearch={mkFun}/>);
+    const btn = search.find('button');
+    btn.simulate('click')
+    expect(mkFun).toHaveBeenCalledTimes(1)
+  });
+
+  it('state should change after click', () => {
+    const mkFun = jest.fn()
+    wrapper = mount(<Search handleSearch={mkFun} />);
+
+    const inputBox = wrapper.find('.search')
+    const button = wrapper.find('button')
+
+    inputBox.simulate('change', {which: 'a'});
+    button.simulate('click');
+
+    expect(wrapper.find('.search')).toBeDefined();
+    expect(wrapper.instance().state.location).toEqual('a')
+  })
+
+
+
+
+
 });
